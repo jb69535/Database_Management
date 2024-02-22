8e368994040b31ba772ffe598b23179a6d9ff4e7
jb69535 <92282867+jb69535@users.noreply.github.com>
2024-02-21 21:03:32 -0500

    Basic setup for project2

---
+++ project2_beom_czech_hwang/my-backend/src/test/java/com/project2_beom_czech_hwang/mybackend/MyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2_beom_czech_hwang.mybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
